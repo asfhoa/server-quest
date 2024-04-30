d08ecea46b67f4ac046102df0f10ea67d0c43df0
asfhoa <91256520+asfhoa@users.noreply.github.com>
2024-04-30 13:50:31 +0900

    1

---
+++ Photon/Assets/Scripts/Npc.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{
+    [SerializeField] string id;
+    [SerializeField] string npcName;
+    [SerializeField] string[] speechs;
+}
